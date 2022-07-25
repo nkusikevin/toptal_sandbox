290eb4788f76b557b92b4fb57c9b09e40eb8b39f
nkusikevin <nkusikvnhart@gmail.com>
2022-07-25 11:39:09 +0200

    first app test

--- challeng_1/src/App.js
+++ challeng_1/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [counter, setCounter] = useState(42);
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+
+				<div>
+					<h2>{counter}</h2>
+					<button onClick={() => setCounter(counter + 1)}>Click</button>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
